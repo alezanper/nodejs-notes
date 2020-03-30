f780523fad74d7d227ca1ea9188748ca1b8fb424
Alexander Benavides <fabenavideso@gmail.com>
2020-03-29 21:01:24 -0500

    using mongodb

---
+++ nodeApiMongo/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+
+dotenv.config();
+
+const port = process.env.PORT || 8080;
+
+//db
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=> console.log("DB connected"));
+
+mongoose.connection.on("error", err => {
+    console.log(`DB Connection error: ${err.message}`);
+});
+
+// Bring routes
+const postRoutes = require('./routes/post');
+
+//const myMiddleware = (req, res, next) => {
+//    console.log("my middleware");
+//    next();
+//};
+
+// Middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use("/", postRoutes);
+
+//app.use(myMiddleware);
+
+app.listen(port, () => {
+    console.log(`A nodejs api is listening on port: ${port}`);
+});
